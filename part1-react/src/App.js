a5c7ece326e264c286416b50d7861e266845a8f0
Sergio <salonsomalagon@gmail.com>
2022-10-08 17:39:40 +0200

    create part2

--- part1-react/src/App.js
+++ part1-react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Mensaje from './Mensaje';
+import { useState } from 'react';
 
 const Descripcion = () => {
     return (
@@ -7,12 +8,38 @@ const Descripcion = () => {
     )
 }
 
+const Counter = ({ contador }) => {
+    return (
+        <h1>{contador}</h1>
+    )
+}
+
+const Button = ({ text, hancleClick }) => {
+    return (
+        <button onClick={hancleClick}>
+            {text}
+        </button>
+    )
+}
+
 const App = () => {
+    const [contador, setContador] = useState(0);
+
+    const createHandleClick = (value) => setContador(value);
+
+    const isEven = contador % 2 === 0;
+    const evenOddMessage = isEven ? 'Es par' : 'Es impar';
+
     return (
         <div className="App">
-            <Mensaje color='red' message='Estamos trabajando' ></Mensaje>
+            <Mensaje color='red' message='Estamos trabajando' />
             <Mensaje color='blue' message='en un curso de react' />
-            <Descripcion></Descripcion>
+            <Descripcion />
+            <Counter contador={contador} />
+            <p><small>{evenOddMessage}</small></p>
+            <Button text='Incrementar' hancleClick={() => createHandleClick(contador + 1)} />
+            <Button text='Decrementar' hancleClick={() => createHandleClick(contador - 1)} />
+            <Button text='Reset' hancleClick={() => createHandleClick(0)} />
         </div>
     );
 }
